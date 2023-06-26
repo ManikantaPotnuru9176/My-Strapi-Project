56e628c8b9f07112166e9db2b7e0d08f24e1528b
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-06-26 10:47:40 +0530

    init: html, css and js

---
+++ index.js
@@ -0,0 +1,34 @@
+fetch("http://localhost:1337/api/products")
+  .then((response) => response.json())
+  .then((data) => {
+    const contentDiv = document.getElementById("content");
+    const products = data.data;
+
+    products.forEach((product) => {
+      const pid = product.attributes.PID;
+      const name = product.attributes.Name;
+      const price = product.attributes.Price;
+      const description = product.attributes.Description;
+      const image = product.attributes.ImageUrl;
+
+      const pageElement = document.createElement("div");
+      pageElement.classList.add("product");
+      pageElement.innerHTML = `
+            <div class="product-image">
+              <img class="product-image" src="${image}" alt="Product Image">
+            </div>
+            <div class="product-info">
+              <h2>${pid}</h2>
+              <p>${name}</p>
+              <p class="price">Price: $${price}</p>
+              <p class="description">${description}</p>
+              <button class="btn">Details</button>
+            </div>
+          `;
+
+      contentDiv.appendChild(pageElement);
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
